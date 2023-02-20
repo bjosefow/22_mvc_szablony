ee532e54557d4993bcf25c29eb60a6c6aa48eebf
Betina Josefowicz <betina.josefowicz@gmail.com>
2023-02-20 19:12:57 +0100

    dodanie projektu

---
+++ bjosefow_22_mvc_szyblony/src/main/java/com/example/bjosefow_22_mvc_szyblony/Bjosefow22MvcSzyblonyApplication.java
@@ -0,0 +1,13 @@
+package com.example.bjosefow_22_mvc_szyblony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bjosefow22MvcSzyblonyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bjosefow22MvcSzyblonyApplication.class, args);
+    }
+
+}
---
+++ bjosefow_22_mvc_szyblony/src/test/java/com/example/bjosefow_22_mvc_szyblony/Bjosefow22MvcSzyblonyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bjosefow_22_mvc_szyblony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bjosefow22MvcSzyblonyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
